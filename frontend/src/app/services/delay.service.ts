d0a94df0962b4f87ba0c2228ea9b3ca37307e2d5
Hannes Wenning <hwen@Hans-Air.fritz.box>
2022-07-05 12:01:04 +0200

    Weitere Änderungen #10

--- frontend/src/app/services/delay.service.ts
+++ frontend/src/app/services/delay.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 import { Delay } from "../interfaces/delay";
+import { zip } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -21,7 +22,7 @@ export class DelayService{
 
     getTimeTables(hours: string[], evaNo: string){
         let tmpDate = new Date();
-        console.log(tmpDate);
+        //console.log(tmpDate);
         let month = tmpDate.getMonth()+1
         let tmpMonth = ""
         if(month < 10) {
@@ -35,7 +36,37 @@ export class DelayService{
         let tmpYear = tmpDate.getFullYear().toString().substring(2,4)
         let date = `${tmpYear}${tmpMonth}${tmpDay}`
         //console.log(evaNo, date, hours);
+        /*
+        let hoursInBlocks: any[] = [];
+        for(let i = 0; i < hours.length / 4; i++){
+            hoursInBlocks.push([hours.splice(i * 4, (i+1)*4)])
+        }*/
+        //console.log(hoursInBlocks);
+        /*
+        return zip(
+            hoursInBlocks.flatMap(
+              (hours) => {
+                this.sleep(2000);
+                return this.httpClient.get<any[]>(`${environment.apiUrl}/delays/getTimeTables/${evaNo}/${date}/${hours}`);
+              }
+            ),
+            (...results: any[]) => {
+                return results //.map(result => result.data);
+              }
+          )
+          */
+          
+
         return this.httpClient.get<any[]>(`${environment.apiUrl}/delays/getTimeTables/${evaNo}/${date}/${hours}`);
+
     }
+
+    sleep(milliseconds: number) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+          currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+      }
 
 }
